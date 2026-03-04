d50961ec4af5871d0caca16af8efdfe57f4c1edf
Fikar Penemuan 2428250070 <fikarpenemuan_2428250070@mhs.mdp.ac.id>
2026-03-04 12:52:56 +0700

    4

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,61 @@
+import 'package:film/screens/favorite_screen.dart';
 import 'package:film/screens/home_screen.dart';
+import 'package:film/screens/search_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Film',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+      title: 'Pilem',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+  @override
+  MainScreenState createState() => MainScreenState();
+}
+
+class MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const SearchScreen(),
+    const FavoriteScreen(),
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite',
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,29 +3,36 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   static const String baseUrl = "https://api.themoviedb.org/3";
-  static const String apikey = "601c97dee4463005302672ef1d4a87f8";
-
+  static const String apiKey = '10c18dfa53630f23e70c0fa063de6456';
   Future<List<Map<String, dynamic>>> getAllMovies() async {
-    final Response = await http.get(
-      Uri.parse("$baseUrl/movie/now_playing?api_key=$apikey"),
+    final response = await http.get(
+      Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"),
     );
-    final data = json.decode(Response.body);
+    final data = json.decode(response.body);
     return List<Map<String, dynamic>>.from(data['results']);
   }
 
   Future<List<Map<String, dynamic>>> getTrendingMovies() async {
-    final Response = await http.get(
-      Uri.parse("$baseUrl/trending/movie/week?api_key=$apikey"),
+    final response = await http.get(
+      Uri.parse("$baseUrl/trending/movie/week?api_key=$apiKey"),
     );
-    final data = json.decode(Response.body);
+    final data = json.decode(response.body);
     return List<Map<String, dynamic>>.from(data['results']);
   }
 
   Future<List<Map<String, dynamic>>> getPopularMovies() async {
-    final Response = await http.get(
-      Uri.parse("$baseUrl/movie/popular?api_key=$apikey"),
+    final response = await http.get(
+      Uri.parse("$baseUrl/movie/popular?api_key=$apiKey"),
     );
-    final data = json.decode(Response.body);
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+
+  Future<List<Map<String, dynamic>>> searchMovies(String query) async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/search/movie?query=$query&api_key=$apiKey"),
+    );
+    final data = json.decode(response.body);
     return List<Map<String, dynamic>>.from(data['results']);
   }
 }
